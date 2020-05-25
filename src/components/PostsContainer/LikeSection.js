5f4b3919ccdcb3efc1beea696d1f0163065760e3
slroberts <shomariroberts@gmail.comclear>
2020-05-24 20:26:31 -0400

    Completed MVP

--- src/components/PostsContainer/LikeSection.js
+++ src/components/PostsContainer/LikeSection.js
@@ -2,26 +2,26 @@
 // console.log your props and see how to add likes to this component so that it will update when you click the icon.
 // HINT: You will need to add an onClick event pass through your props and update the hard coded Likes
 import React from "react";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faComment, faHeart } from '@fortawesome/free-regular-svg-icons'
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faComment, faHeart} from "@fortawesome/free-regular-svg-icons";
 
-const LikeSection = props => {
+const LikeSection = (props) => {
   return (
     <div>
-      <div
-        className="like-section"
-        key="likes-icons-container"
-      >
-        <div className="like-section-wrapper">
-          <FontAwesomeIcon icon={faHeart} />
+      <div className="like-section" key="likes-icons-container">
+        <div className="like-section-wrapper" onClick={props.handleLikes}>
+          {props.showHeart}
         </div>
         <div className="like-section-wrapper">
-          <FontAwesomeIcon icon={faComment} />
+          {/* <FontAwesomeIcon icon={faComment} /> */}
+          💬
         </div>
       </div>
-      <p className="like-number">27 likes</p>
+      <p className="like-number">
+        {props.showLikes} {props.likeOrLikes}
+      </p>
     </div>
-  )
+  );
 };
 
 export default LikeSection;
--- src/components/PostsContainer/Post.js
+++ src/components/PostsContainer/Post.js
@@ -1,22 +1,49 @@
 // You will add code in this file
-import React from "react";
+import React, {useState} from "react";
 import CommentSection from "../CommentSection/CommentSectionContainer";
 import LikeSection from "./LikeSection";
 import PostHeader from "./PostHeader";
+import TimeStamp from "../PostsContainer/TimeStamp";
 
 import "./Posts.css";
 
 // pass props in this file to
-const Post = props => {
+const Post = (props) => {
   // set up state for the likes
+  const [like, setLikes] = useState(false);
+  const [likesNum, setLikesNum] = useState(props.post.likes);
+  const [likeOrLikes, setLikeOrLikes] = useState("like");
+
+  const [changeHeart, setChangeHeart] = useState(
+    // <FontAwesomeIcon icon={faHeart} />
+    "🖤"
+  );
+
+  const handleLikesUpdate = () => {
+    if (!like) {
+      setLikes(true);
+      setLikesNum(likesNum + 1);
+      setChangeHeart("❤️");
+    } else if (like) {
+      setLikes(false);
+      setLikesNum(likesNum - 1);
+      setChangeHeart("🖤");
+    }
+  };
+
+  const updateLikeOrLikes = () => {
+    if (likesNum > 1) {
+      setLikeOrLikes("likes");
+    }
+  };
+
+  window.addEventListener("load", updateLikeOrLikes);
 
   return (
     <div className="post-border">
       <PostHeader
         username={props.post.username}
-        thumbnailUrl={
-          props.post.thumbnailUrl
-        }
+        thumbnailUrl={props.post.thumbnailUrl}
       />
       <div className="post-image-wrapper">
         <img
@@ -25,11 +52,18 @@ const Post = props => {
           src={props.post.imageUrl}
         />
       </div>
-      <LikeSection />
+      <LikeSection
+        showHeart={changeHeart}
+        showLikes={likesNum}
+        likeOrLikes={likeOrLikes}
+        handleLikes={() => handleLikesUpdate()}
+      />
+
       <CommentSection
         postId={props.post.imageUrl}
         comments={props.post.comments}
       />
+      <TimeStamp timestamp={props.post.timestamp} />
     </div>
   );
 };
